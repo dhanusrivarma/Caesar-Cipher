d1b1a2ab08533d4bc49319149b25a30a377a0e61
Dhanu Sri Varma <chandikadhanusri@gmail.com>
2026-08-10 21:51:37 +0530

    Launch application through login

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
 import subprocess
 import sys
-import os
 
-
-def main():
-    login_file = os.path.join(
-        os.path.dirname(os.path.abspath(__file__)),
-        "login.py"
-    )
-
-    subprocess.run([sys.executable, login_file])
-
-
-if __name__ == "__main__":
-    main()
+subprocess.run([sys.executable, "login.py"])
